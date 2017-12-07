68313b7cac8a09efb7e591fa5eb60b44a1efb9ed
ExaconAT <33482537+ExaconAT@users.noreply.github.com>
2017-12-07 13:39:14 +0100

    Create renew.sh

---
+++ renew.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SITES=(xxx.xxx.com yyy.yyy.com)
+EMAIL=your@email.com
+
+for DOMAIN in "${SITES[@]}"
+do
+        /usr/bin/certbot certonly --standalone --agree-tos --non-interactive -m $EMAIL -d $DOMAIN --preferred-challenges http --http-01-port 54321
+
+        # move to the correct let's encrypt directory
+        cd /etc/letsencrypt/live/$DOMAIN
+        cat fullchain.pem privkey.pem > /etc/haproxy/certs/$DOMAIN.pem
+done
+# reload haproxy
+service haproxy reload
